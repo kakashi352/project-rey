d9403c9cae28c2974eab3f66848be973fff6d28f
Zfxj7 <99494069+Zfxj7@users.noreply.github.com>
2023-07-30 02:22:07 +0300

    Update generate-pdf.php

--- generate-pdf.php
+++ generate-pdf.php
@@ -39,59 +39,190 @@ if (isset($_GET["pdf"])) {
     $_SESSION['note'] = $note;
     $_SESSION['select'] = $select;
 
-    header("Location:template.php");
-}
-
-if (isset($_GET["send"])) {
-
-
-    //Load Composer's autoloader
-
+    
     //Create an instance; passing `true` enables exceptions
     $mail = new PHPMailer(true);
-
+    // $mail->setLanguage('ar' , 'PHPMailer/language/phpmailer.lang-ar.php');
+    $mail->CharSet = 'UTF-8';
     try {
         //Server settings
         $mail->isSMTP();                                            //Send using SMTP
-
+        
         $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
-
+        
         $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-
+        
         $mail->Username   = 'doctors.drs1@gmail.com';                     //SMTP username
-
+        
         $mail->Password   = 'wqgkurnthxkpggvc';                               //SMTP password
-
+        
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-
+        
         $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
-
+        
         //Recipients
-
+        
         $mail->setFrom('doctors.drs1@gmail.com', 'وزارة الري و الصرف'); //from
-
-        $mail->addBCC('7ssanie2014@gmail.com');                     //Name is optional
-
+        
+        $mail->addBCC('somo7738@gmail.com');  //<-- غير هنا                   //Name is optional
+        
         $mail->isHTML(true);     
         
-        $mail->addAttachment('template1.pdf' , 'template1.pdf');//Set email format to HTML
+        $mail->addAttachment('template.php' , 'template.php');//Set email format to HTML
+        
+        $mail->Subject = 'وزارة الري و الصرف' ;
+        
+        $mail->Body    = "
 
-        $mail->Subject = ' hi bitch وزارة الري و الصرف' ;
+        <!DOCTYPE html>
+        <html>  
+        <head>
+          <link rel=stylesheet href=./style/template.css />
+        
+        </head>   
+        <body dir=rtl>
+          <header  >
+            <h1 style=text-align: center;>
+              <div class=blob></div>
+              <div class=blob-white></div>
+              اللجنة التنفيذية <br />
+              للمحافظة على الواحة
+            </h1>
+            
+          </header>
+        
+        
+          <div style=text-align: center;>
+            <h1 style=text-align: center; margin:.5em;><?php echo $_SESSION[select] ?></h1>
+            <article>
+              <div class=compine>
+                <p> اشارة الى خطاب محافظ الاحساء وتاريخ 1441/4/5<span>هـ</span> </p>
+                <p> بشأن الجولة الميدانية </p>
+                <p>ليوم
+                  <?php
+                  if ($_SESSION[day] == 0) {
+                    echo الاحد;
+                  }
+                  if ($_SESSION[day] == 1) {
+                    echo الاثنين;
+                  }
+                  if ($_SESSION[day] ==2 ) {
+                    echo الثلاثاء;
+                  }
+                  if ($_SESSION[day] == 3) {
+                    echo الاربعاء;
+                  }
+                  if ($_SESSION[day] == 4) {
+                    echo الخميس;
+                  }
+                  if ($_SESSION[day] == 5) {
+                    echo الجمعة;
+                  }
+                  if ($_SESSION[day] == 6) {
+                    echo السبت;
+                  }
+                  ?> </p>
+                <p> الموافق <?php echo $_SESSION[year]./.$_SESSION[date]./.$_SESSION[day] ?> </p>
+                <p> ساعة <?php echo $_SESSION[time] ?> </p>
+                <p>وبحضور لجنة الميدانية للمحافظة على الواحة من</p>
+                <p>,التعديات وعليها</p>
+                <p>تم الوقوف على المنشأة <?php echo $_SESSION[Company] ?></p>
+                <p>والواقعة في <?php echo $_SESSION[City] ?></p>
+                <p>والعائد للمواطن <?php echo $_SESSION[Citizin] ?></p>
+                <p>رقم (سجل / إقامة) <?php echo $_SESSION[National] ?></p>
+                <p>رقم الجوال: <?php echo $_SESSION[Phone] ?></p>
+                <p>ملاحضات <?php echo $_SESSION[note] ?></p>
+              </div>
+        
+              <hr style=width: 100%; margin: 2em 0; />
+        
+              <h1 dir=rtl><u>أعضاء اللجنة الميدانية</u></h1>
+              <div class=members >
+                <p class=p14>
+                  <b>مندوب مكتب العمل </b>
+                  <br />
+                  مرتضى امين البقشي
+                </p>
+                <p class=p15>
+                  <b>مندوب الأمانة</b>
+                  <br />
+                  احمد سعد الذياب
+                </p>
+                <p class=p16>
+                  <b>مندوب المؤسسة العامة للري </b>
+                  <br />
+                  م.فهد عبدالرحمن الذكرالله
+                  <br />
+                  عبدالحكيم عبدالله البريك
+                </p>
+                <p class=p17>
+                  <b>مندوب وزارة البيئة والمياه والزراعة </b>
+                  <br />
+                  م.احمد علي العباد
+                </p>
+                <p class=p18>
+                  <b>مندوب الشرطة </b>
+                  <br />
+                  رقيب أول/خالد سعد المحارفي
+                </p>
+                <p class=p19>
+                  <b>مندوب وزارة التجاة بالأحساء </b>
+                  <br />
+                  سمير عبدالوهاب البراهيم
+                </p>
+                <p class=p20>
+                  <b>مندوب الدفاع المدني </b>
+                  <br />
+                  الرقيب/عبدالله سالم بالطيور
+                </p>
+              </div>
+              <hr style=width: 100%; margin: 2em 0; />
+              <ul dir=rtl>
+                <li>نموذج اغلاق: تعني إغلاق المنشأة وعدم مزاولة النشاط المخالف.</li>
+                <li>
+                  نموذج فتح: تعني فتح المنشأة بحضور اعضاء لجنة المحافظة على الواحة.
+                </li>
+                <li>
+                  تنبيه: معاودة النشاط المخالف يعرضك لقطع التيار الكهربائي والمسألة
+                  القانونية.
+                </li>
+                <li>
+                  تنويه: مراجعة مقر اللجنة بالمؤسسة العامة للري (لجنة المحافظة على
+                  الواحةالزراعية من التعديات) كل اربعاء من 8:30 إلى 12ظهراً.
+                </li>
+                <li>
+                  اثناءالمراجعة: يتم احضار المستندات الثبوتية لصاحب المنشأة ومنها
+                  بطاقة الاحوال أو من ينوب عنه بوكالة الشرعية . <br />
+                  للاستفسارات الاتصال على رقم جوال (0504847856).(0583305484)
+                </li>
+              </ul>
+            </article>
+          </div>
+        
 
-        $mail->Body    = 'سوف يتم <b>تفجير عبود!</b>';
-
+        </body>
+        
+        </html>
+        ";
+        
         $mail->send();
-
+        
+        header("Location:template.php");
         echo 'Message has been sent';
     } catch (Exception $e) {
-
+        
         echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
     }
 }
 
 
 
+// <h1 style=text-align: center ; > السلام عليكم ورحمة الله وبركاته </h1>
 
+// <p style=text-align: right ; > تم ارسال $select </p>
+
+
+// <p> </p>
 
 
 
